fd099c828ae6a5eb82211ef6d9b92588376580b1
shivam01912 <shivam01912@gmail.com>
2022-09-27 13:52:56 +0530

    added redis board test and integration test.

--- src/test/java/com/intuit/leaderboard/board/RedisBoardTest.java
+++ src/test/java/com/intuit/leaderboard/board/RedisBoardTest.java
@@ -1,84 +1,53 @@
 package com.intuit.leaderboard.board;
 
-import com.intuit.leaderboard.TestConfig;
-import com.intuit.leaderboard.TestUtil;
 import com.intuit.leaderboard.model.Player;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Tuple;
-
-import java.util.List;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@Import(TestConfig.class)
 class RedisBoardTest {
 
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        public Board testRedisBoard() {
+            return new RedisBoard();
+        }
+    }
     private final String KEY = "Leaderboard";
 
     @Autowired
     private Board testRedisBoard;
 
+    @MockBean
     private StringRedisTemplate redisTemplate;
-
-    /**
-     * This method runs before each test case and clears the leaderboard for accurate testing.
-     */
-    @BeforeEach
-    void setup() {
-        try (Jedis jedis = new Jedis()) {
-          jedis.flushAll();
-        }
-    }
 
     /**
      * Tests happy path scenario where a Player is added to the leaderboard.
      */
     @Test
     void testUpdateBoardHappyPath() {
+        ZSetOperations mockOps = Mockito.mock(ZSetOperations.class);
+        Mockito.when(redisTemplate.opsForZSet()).thenReturn(mockOps);
+        Mockito.when(mockOps.add(Mockito.anyString(), Mockito.anyString(), Mockito.anyDouble()))
+                .thenReturn(true);
+
         Player testPlayer = new Player("TestPlayer", 100);
         testRedisBoard.updateBoard(testPlayer);
 
-        try (Jedis jedis = new Jedis()) {
-            Set<Tuple> tuples = jedis.zrangeWithScores(KEY, 0, 0);
-            for(Tuple t: tuples) {
-                assertEquals(testPlayer.getName(), t.getElement());
-                assertEquals(testPlayer.getScore(), (int)t.getScore());
-            }
-        }
-    }
 
-    /**
-     * Tests if we get a correct size and order of leaderboard.
-     */
-    @Test
-    void testGetTopPlayersHappyPath() {
-        TestUtil.addData(testRedisBoard, 10);
-        List<Player> topPlayers = testRedisBoard.getTopPlayers(5);
-
-        assertEquals(5, topPlayers.size());
-        TestUtil.assertOrder(topPlayers);
-    }
-
-    /**
-     * Tests for well handled case of a lesser number of players available in the leaderboard than requested for.
-     */
-    @Test
-    void testGetTopPlayersLessThanKAvailable() {
-        TestUtil.addData(testRedisBoard, 3);
-        List<Player> topPlayers = testRedisBoard.getTopPlayers(5);
-
-        assertEquals(3, topPlayers.size());
-        TestUtil.assertOrder(topPlayers);
+        Mockito.verify(redisTemplate, Mockito.times(1)).opsForZSet();
+        Mockito.verify(mockOps, Mockito.times(1)).add(Mockito.anyString(), Mockito.anyString(), Mockito.anyDouble());
     }
 }
